2981bea4b8fa0b9803e3b5b6b3384c08c8659883
Arpit Ranjan <thebinaryblasters@gmail.com>
2026-03-02 00:16:35 +0530

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
-import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setUserData } from './redux/userSlice'
@@ -21,7 +20,10 @@ function App() {
   useEffect(() => {
     const getUser = async () => {
       try {
-        const result = await axios.get(ServerUrl + "/api/user/current-user", { withCredentials: true })
+        const result = await axios.get(
+          ServerUrl + "/api/user/current-user",
+          { withCredentials: true }
+        )
         dispatch(setUserData(result.data))
       } catch (error) {
         console.log(error)
